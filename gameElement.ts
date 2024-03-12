f741a5cab99c466730e915947c4161a2e0e84904
NogaTal98 <isanyo.noga@gmail.com>
2024-03-12 12:50:22 +0200

    onClick
    add onClic logic

---
+++ gameElement.ts
@@ -0,0 +1,39 @@
+import * as THREE from 'three';
+import Type from './type';
+
+class GameElement extends THREE.Mesh {
+    type: Type;
+    speed: number;
+    direction: number;
+    startTime: number;
+    geometry: THREE.Geometry;
+    material: THREE.Material;
+    position: THREE.position;
+    translateX: THREE.translateX;
+    translateY: THREE.translateY;
+    rotation: THREE.rotation;
+
+    public init_position() {
+        this.position.x = Math.random()*(4-(-4))+(-4);
+        this.position.y = Math.random()*(4-(-4))+(-4);
+        this.position.z = Math.random()*(4-(-4))+(-4);
+    }
+
+    public move() {
+    }
+
+    public update_color() {
+        this.material = new THREE.MeshBasicMaterial( { color: this.type == Type.Collect ? 0x00ff00 : 0xff0000} );
+    }
+
+    public onClick(removeElement: any) {
+        if (this.type == Type.Collect) {
+            removeElement(this);
+        }
+        else {
+            alert("Game Over");
+        }
+    }
+}
+
+export default GameElement;
--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import Box from './box.ts';
 import Sphere from './sphere.ts';
 import Pyramid from './pyramid.ts';
+import GameElement from './gameElement.ts';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -9,6 +10,8 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+const raycaster = new THREE.Raycaster()
+const mouse = new THREE.Vector2()
 
 const number_of_boxes =  Math.floor(Math.random() * (5-1)+1);
 const number_of_spheres = Math.floor(Math.random() * (5-1)+1);
@@ -17,19 +20,39 @@ const number_of_pyramids = Math.floor(Math.random() * (5-1)+1);
 let boxes: Box[] = [];
 for (let i = 0; i < number_of_boxes; i++) {
 	boxes.push(new Box());
-	scene.add(boxes[i].shape);
+	scene.add(boxes[i]);
 }
 
 let spheres: Sphere[] = [];
 for (let i = 0; i < number_of_spheres; i++) {
 	spheres.push(new Sphere());
-	scene.add(spheres[i].shape);
+	scene.add(spheres[i]);
 }
 
 let pyramids: Pyramid[] = [];
 for (let i = 0; i < number_of_pyramids; i++) {
 	pyramids.push(new Pyramid());
-	scene.add(pyramids[i].shape);
+	scene.add(pyramids[i]);
+}
+
+let width = 0
+let height = 0
+let intersects = []
+
+window.addEventListener('pointermove', (e) => {
+	mouse.set((e.clientX / window.innerWidth) * 2 - 1, - (e.clientY / window.innerHeight) * 2 + 1)
+	raycaster.setFromCamera(mouse, camera)
+	intersects = raycaster.intersectObjects(scene.children, true)
+})
+
+window.addEventListener('click', (e) => {
+	intersects.forEach((hit: any) => {
+		hit.object.onClick(() => removeElement(hit.object))
+	})
+})
+
+function removeElement(element: GameElement) {
+	scene.remove(element)
 }
 
 camera.position.z = 5;
